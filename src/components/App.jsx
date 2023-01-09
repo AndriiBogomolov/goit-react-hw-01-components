ac3f3c3cb70d48ac4a66f07ee7654fcaff4b64c9
Andrii Bogomolov <bogomolov.andrii@gmail.com>
2023-01-10 00:57:48 +0100

    Profile added

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,49 @@
-export const App = () => {
+import { Profile } from 'components/Profile/Profile';
+import user from './Profile/user.json';
+
+export const App = ( ) => {
   return (
     <div
       style={{
-        height: '100vh',
         display: 'flex',
+        flexDirection: 'column',
         justifyContent: 'center',
         alignItems: 'center',
         fontSize: 40,
-        color: '#010101'
+        color: '#010101',
       }}
     >
-      React homework template
+      <Profile
+        avatar={user.avatar}
+        username={user.username}
+        tag={user.tag}
+        location={user.location}
+        stats={user.stats} />
+      
+
+
+
+
     </div>
+
   );
 };
+
+
+
+// export const App = ({ children }) => {
+//   return (
+//     <div
+//       style={{
+//         display: 'flex',
+//         flexDirection: 'column',
+//         justifyContent: 'center',
+//         alignItems: 'center',
+//         fontSize: 40,
+//         color: '#010101',
+//       }}
+//     >
+//       { children }
+//     </div>
+//   );
+// };
